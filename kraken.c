5fbbeddf157699df670fe0e042edfec022a772a1
Rafał Słota <rafal.slota@erlang-solutions.com>
2020-02-08 02:28:30 +0100

    fixup! fixup! fixup! fixup! fixup! Adjust default pwm curves

--- kraken.c
+++ kraken.c
@@ -53,7 +53,7 @@ struct kraken_status {
     u8 unknown_1;       // 03 or 02 or possibly other values
     u8 fan_rpm[2];      // big-endian
     u8 pump_rpm[2];     // big-endian
-    u8 unknown_2[10];   // 00 00 00 3f 02 00 01 08 1e 00
+    u8 unknown_2[20];   // 00 00 00 3f 02 00 01 08 1e 00
 };
 
 struct kraken_autopoint {
@@ -119,11 +119,10 @@ static int kraken_receive_message(struct usb_kraken *kraken,
     int retval;
 
     memset(message, 0, expected_length * 2);
-    u8 *tmp = malloc(4000);
 
     retval = usb_bulk_msg(kraken->udev,
                               usb_rcvintpipe(kraken->udev, 0x81),
-                              tmp, expected_length, &received, 3000);
+                              message, expected_length, &received, 3000);
 
     if (unlikely(received != expected_length)) {
         dev_warn_ratelimited(
